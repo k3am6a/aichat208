17d16b8c6044ef4da9d47f0860585a1d1141f256
k3am6a <140267951+k3am6a@users.noreply.github.com>
2023-07-23 22:52:41 +0800

    Update server.ts

--- app/config/server.ts
+++ app/config/server.ts
@@ -37,8 +37,11 @@ export const getServerSideConfig = () => {
     );
   }
 
+  const apiKeys = (process.env.OPENAI_API_KEY ?? '').split(',')
+  const apiKey = apikeys.at(Math.floor(Math.random() * apikeys.length)) ?? ''
+  
   return {
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey,
     code: process.env.CODE,
     codes: ACCESS_CODES,
     needCode: ACCESS_CODES.size > 0,
